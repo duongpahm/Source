c1a1bd67deca7b6f0fe8c43a3e967a4a7e674cce
duongpahm <lamduongp308@gmail.com>
2024-09-17 23:20:05 +0700

    add file

--- bai5.c
+++ bai5.c
@@ -15,7 +15,7 @@ void fibo(int n){
         fibo[i] = fibo[i - 1] + fibo[i - 2];
     }
     for(int i = 0; i < n; i++){
-            printf("%d ", fibo[i]);
+            printf("%lld ", fibo[i]);
     }
 
 }
